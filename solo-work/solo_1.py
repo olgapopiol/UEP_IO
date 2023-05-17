78788d17adadc0fced9ac7725a6ee86ff707f14b
Olga Popiol <popolga@op.pl>
2023-05-17 15:12:56 +0200

    zadanie 1.8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -50,3 +50,13 @@ print("Alfabetyczna lista studentow wynosi: ")
 for student in studenci:
     print(student)
 
+
+
+# zadanie 1.8
+# posortuj alfabetycznie (od nazwiska) studentow
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci.sort(key=lambda x: x.split()[-1])
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
+
