dd263da0f0032b46274683e4f722fcc316a6a667
Olga Popiol <popolga@op.pl>
2023-05-17 14:56:39 +0200

    zadanie 1.2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,7 +1,11 @@
 # zadanie 1.1
-
 hello = "Hello"
 student = "Ola"
 # oczekiwany rezultat: Hello Ola
 # wykorzystaj w princie zmienne hello i student
 print("{} {}".format(hello, student))
+
+
+# zadanie 1.2
+student = input("Wpisz swoje imie")
+print(f"Hello {student}")
