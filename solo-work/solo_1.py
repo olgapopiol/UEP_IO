770ce24b151532deef194b628485696450761605
Olga Popiol <popolga@op.pl>
2023-05-17 15:10:09 +0200

    zadanie 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -31,3 +31,12 @@ potega = 4
 wynik = liczba ** potega
 
 print("Wynik wynosi: ", wynik)
+
+
+#zadanie 1.6
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
